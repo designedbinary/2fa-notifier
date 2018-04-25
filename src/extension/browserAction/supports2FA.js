a634743692a74fe0c065ae3c3ba8eff242e244b7
Conor Gilsenan <conorgilsenan@gmail.com>
2018-04-25 00:17:27 -0400

    Wait for the DOM to load before executing JS
    n00b mistake. We were executing our JS before the DOM
    finished loading in the page, so the button was not found
    because it hadn't been created yet.
    
    Details in the docs:
    https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded

--- src/extension/browserAction/supports2FA.js
+++ src/extension/browserAction/supports2FA.js
@@ -1,10 +1,12 @@
-let more2faInfoButton = document.getElementById('more2faInfoButton');
+document.addEventListener('DOMContentLoaded', function (event) {
+  let more2faInfoButton = document.getElementById('more2faInfoButton');
 
-more2faInfoButton.addEventListener('click', function () {
-  var more2faInfo = document.getElementById('methods');
-  if (more2faInfo.style.display === 'block') {
-    more2faInfo.style.display = 'none';
-  } else {
-    more2faInfo.style.display = 'block';
-  }
+  more2faInfoButton.addEventListener('click', function () {
+    var more2faInfo = document.getElementById('methods');
+    if (more2faInfo.style.display === 'block') {
+      more2faInfo.style.display = 'none';
+    } else {
+      more2faInfo.style.display = 'block';
+    }
+  });
 });
