678062f81dc2a923800cafa4abcd4e3e5092a0f4
Ray <designedbinary@users.noreply.github.com>
2018-04-26 20:06:32 -0700

    first pass
    Got most of the design up as intended. Waiting to review with Conor before moving further and adding in animation.

--- src/extension/browserAction/supports2FA.js
+++ src/extension/browserAction/supports2FA.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+/*document.addEventListener('DOMContentLoaded', function (event) {
   let more2faInfoButton = document.getElementById('more2faInfoButton');
+  let windowHeight = document.body.clientHeight;
 
   more2faInfoButton.addEventListener('click', function () {
     var more2faInfo = document.getElementById('methods');
@@ -9,4 +10,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
       more2faInfo.style.display = 'block';
     }
   });
+
+});/* */
+
+$(function(){
+
+  let originalWindowHeight = $(document).height();
+
+  $("#more2faInfoButton").click(function() {
+    
+    $(".methods").toggle();
+
+    if ($(".methods").css("display") === 'none') {
+      $("html").css("height", originalWindowHeight);
+      console.log($(".methods").css("display"));
+      console.log("original window height = " + originalWindowHeight);
+    }
+  });
+
 });
